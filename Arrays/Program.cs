2d70bb9aaa936ea450544a292f02ae49bc4a1c0e
muriminjue <muriminjue.nm@gmail.com>
2023-06-02 13:30:38 +0300

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // c# arrays should be the same type
+            int[] luckyNumber = { 4, 6, 7, 8, 9, 12, 25, 39 };
+            string[] friends = new string[5]; // must have length of array
+
+            Console.WriteLine(luckyNumber[2]);
+
+            luckyNumber[2] = 45;
+            Console.WriteLine(luckyNumber[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
