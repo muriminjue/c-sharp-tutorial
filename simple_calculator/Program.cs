b089695422c2929c50e91297cfd17ced9dc1c8a0
muriminjue <muriminjue.nm@gmail.com>
2023-06-02 12:22:26 +0300

    calc

---
+++ simple_calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The sum is: " + (num1 + num2));
+            Console.ReadLine();
+
+        }
+    }
+}
