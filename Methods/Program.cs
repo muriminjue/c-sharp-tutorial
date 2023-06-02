1cb5245fc609c2f455fefc13ab62e6811feb6d51
muriminjue <muriminjue.nm@gmail.com>
2023-06-02 13:47:24 +0300

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("mike", 21);
+            SayHi("John", 29);
+            SayHi("Duke", 31);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine(name + " is " + age + " years old");
+        }
+    }
+}
