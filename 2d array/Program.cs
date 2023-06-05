2e227b901823580ba7fc1e8beb201d8cb11b91fb
muriminjue <muriminjue.nm@gmail.com>
2023-06-05 19:41:23 +0300

    more arrays

--- 2d array/Program.cs
+++ 2d array/Program.cs
@@ -10,6 +10,20 @@ namespace _2d_array
     {
         static void Main(string[] args)
         {
+            int[,] numberGroup =
+             {
+                {1,2 },
+                {2,4 },
+                {5,6 }
+            };
+
+            int[,] numberGroup1 = new int[3 /*Rows */, 4 /*Columns */];
+
+            Console.WriteLine(numberGroup[1,0]);
+            Console.WriteLine(numberGroup[1,1]);
+            Console.WriteLine(numberGroup[2,1]);
+
+            Console.ReadLine();
         }
     }
 }
