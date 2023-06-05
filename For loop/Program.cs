a555a677bef0c8b5277d24d6e9b40ac5bc7bbd2f
muriminjue <muriminjue.nm@gmail.com>
2023-06-05 13:41:53 +0300

    For loop

---
+++ For loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i< 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
