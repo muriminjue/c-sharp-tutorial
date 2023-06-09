2bdc2068ce19c97bd40cbbf460360c9223a3f02d
muriminjue <muriminjue.nm@gmail.com>
2023-06-09 10:50:18 +0300

    Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+
+            book1.title = "Jake and Jill";
+            book1.author = "Fred Long";
+            book1.pages = 95;
+
+            Console.WriteLine(book1.author);
+
+            Console.ReadLine();
+        }
+    }
+}
